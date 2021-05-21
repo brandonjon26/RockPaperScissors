1d3b549d4b3aacfbbd8543e77d5b74266582f0ba
Brandon Hill <brandonjon26@gmail.com>
2021-05-21 15:08:19 -0500

    the game works

---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+void RockHand(int pick)
+{
+    if (pick == 1)
+    {
+
+        Console.WriteLine(@"
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+    ");
+    }
+    if (pick == 2)
+    {
+        Console.WriteLine(@"
+     _______
+-- - '  ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+    ");
+    }
+    if (pick == 3)
+    {
+        Console.WriteLine(@"
+    _______
+-- - '   ____)____
+            ______)
+       __________)
+      (____)
+-- -.__(___)
+    ");
+    }
+
+}
+
+
+int myScore = 0;
+int computerScore = 0;
+Play();
+void Play()
+{
+    Console.Clear();
+    if (myScore == 3)
+    {
+        Console.WriteLine("Winner!!!");
+    }
+    else if (computerScore == 3)
+    {
+        Console.WriteLine("Loser!");
+    }
+    else
+    {
+        Console.WriteLine("-----------------------------");
+        Console.WriteLine($"|Player: {myScore} | Computer: {computerScore} |");
+        Console.WriteLine("-----------------------------");
+        Console.WriteLine("Make a choice...");
+        Console.WriteLine("1) Rock");
+        Console.WriteLine("2) Paper");
+        Console.WriteLine("3) Scissors");
+
+
+
+        string pick = Console.ReadLine();
+        int myPick = Int32.Parse(pick);
+
+        Random i = new Random();
+        int computerPick = i.Next(1, 3);
+        RockHand(myPick);
+        Console.WriteLine("VS");
+        RockHand(computerPick);
+        System.Threading.Thread.Sleep(750);
+
+        if (myPick == computerPick)
+        {
+            Play();
+        }
+        else if (myPick == 1)
+        {
+            if (computerPick == 2)
+            {
+                computerScore++;
+                Play();
+            }
+            else
+            {
+                myScore++;
+                Play();
+            }
+        }
+        else if (myPick == 2)
+        {
+            if (computerPick == 3)
+            {
+                computerScore++;
+                Play();
+            }
+            else
+            {
+                myScore++;
+                Play();
+            }
+        }
+        else if (myPick == 3)
+        {
+            if (computerPick == 1)
+            {
+                computerScore++;
+                Play();
+            }
+            else
+            {
+                myScore++;
+                Play();
+            }
+        }
+    }
+
+}
